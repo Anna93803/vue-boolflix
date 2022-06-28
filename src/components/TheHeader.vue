9ffcb0521eea9032969b3a2bcab84e87c4a0000c
Anna93803 <anna.tassone93@gmail.com>
2022-06-28 15:46:03 +0200

    add Milestone 1

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,10 +1,15 @@
 <template>
     <div>
         <input type="text" placeholder="Search" v-model="searchText">
-        <button>Send</button>
+        <button @click="getMovies">Send</button>
 
         <ul>
-            <li v-for="(movie, i) in movieList" :key="i"> {{movie}}</li>
+            <li v-for="(movie, i) in movieList" :key="i"><h2>{{movie.original_title}}</h2> 
+            <h5>{{movie.title}}</h5>
+            <small>{{movie.original_language}}</small>
+            <h6>{{movie.vote_average}}</h6>
+            
+            </li>
         </ul>
     </div>
 </template>
@@ -22,6 +27,7 @@ export default {
     },
     methods: {
         getMovies() {
+
             axios.get("https://api.themoviedb.org/3/search/movie", {
                 params: {
                     api_key: "52fa4879a16a66a273c3d46682a089b6",
@@ -35,7 +41,6 @@ export default {
             .cacht(() => {
                 alert('Operazione non riuscita.')
             })
-            
         }
     },
 }
